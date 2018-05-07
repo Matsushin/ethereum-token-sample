28fb8c0433b00fd2fdeb0cec0314ed1a949476d5
matsushin11 <matsu1983@gmail.com>
2018-05-07 22:11:41 +0900

    リファクタ

--- app/models/ethereum_api.rb
+++ app/models/ethereum_api.rb
@@ -4,32 +4,32 @@ class EthereumApi
   attr_accessor :contract, :client
 
   def initialize
-    @client = Ethereum::HttpClient.new(GANACHE_URL)
+    self.client = Ethereum::HttpClient.new(GANACHE_URL)
     create_default_token! if Token.count.zero?
     token = Token.first
-    @contract = Ethereum::Contract.create(name: "AppToken", truffle: { paths: [ TRUFFLE_PATH ] }, client: @client, address: token.token_address)
+    self.contract = Ethereum::Contract.create(name: "AppToken", truffle: { paths: [ TRUFFLE_PATH ] }, client: @client, address: token.token_address)
   end
 
   def create_default_token!
-    @client = Ethereum::HttpClient.new(GANACHE_URL)
-    @contract = Ethereum::Contract.create(name: "AppToken", truffle: { paths: [ TRUFFLE_PATH ] }, client: @client)
+    self.client = Ethereum::HttpClient.new(GANACHE_URL)
+    self.contract = Ethereum::Contract.create(name: "AppToken", truffle: { paths: [ TRUFFLE_PATH ] }, client: @client)
     token = Token.new(
       name: Token::DEFAULT_NAME,
       symbol: Token::DEFAULT_SYMBOL,
       total_supply: Token::DEFAULT_TOTAL_SUPPLY
     )
-    address = @contract.deploy_and_wait(token.name, token.symbol, token.total_supply)
+    address = contract.deploy_and_wait(token.name, token.symbol, token.total_supply)
     token.token_address = address
     token.save!
   end
 
   def get_accounts
-    @client.eth_accounts['result']
+    client.eth_accounts['result']
   end
 
   def transfer(address, amount)
     begin
-      @contract.transact_and_wait.transfer(address, amount)
+      contract.transact_and_wait.transfer(address, amount)
     rescue => e
       Rails::logger::debug("Error token transfer. message: #{e.message}")
       false
